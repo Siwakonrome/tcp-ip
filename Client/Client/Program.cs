3200cffce1c5cbcada17b40c527e47b325558025
Siwakon <tiger.prasang@gmail.com>
2022-07-01 14:48:03 +0700

    Update

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -2,13 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Sockets;
 
 namespace Client
 {
     class Program
     {
+        // ipconfig
+        const int PORT_NO = 80;
+        const string SERVER_IP = "10.61.5.25";
+
         static void Main(string[] args)
         {
+            //---data to send to the server---
+            string textToSend = DateTime.Now.ToString();
+            //---create a TCPClient object at the IP and port no.---
+            TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
+            NetworkStream nwStream = client.GetStream();
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+            //---send the text---
+            Console.WriteLine("Sending : " + "This Position");
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+            //---read back the text---
+            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+            Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+            Console.ReadLine();
+            client.Close();
         }
     }
 }
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -2,13 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Server
 {
     class Program
     {
+        const int PORT_NO = 80;
+        const string SERVER_IP = "10.61.5.25";
+
         static void Main(string[] args)
         {
+            //---listen at the specified IP and port no.---
+            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+            TcpListener listener = new TcpListener(localAdd, PORT_NO);
+            Console.WriteLine("Listening...");
+            listener.Start();
+            //---incoming client connected---
+            TcpClient client = listener.AcceptTcpClient();
+            //---get the incoming data through a network stream---
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+            //---read incoming stream---
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+            //---convert the data received into a string---
+            string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received : " + dataReceived);
+            //---write back the text to the client---
+            Console.WriteLine("Sending back : " + "Received This Position");
+            nwStream.Write(buffer, 0, bytesRead);
+            client.Close();
+            listener.Stop();
+            Console.ReadLine();
         }
     }
 }
