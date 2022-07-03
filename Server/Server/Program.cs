076db5f8a07edc833fa5b450a93d7c03b3c982d1
siwakon rommueang <siwakon.prasang@mail.kmutt.ac.th>
2022-07-03 17:01:44 +0700

    Update

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -11,6 +11,7 @@ namespace Server
     {
         const int PORT_NO = 80;
         const string SERVER_IP = "192.168.101.129";
+        public const byte ESC_ASCII_VALUE = 0x1b;
 
         static void Main(string[] args)
         {
@@ -20,18 +21,27 @@ namespace Server
             Console.WriteLine("Server Start...");
             while (true)
             {
+                Console.WriteLine("Press any key to continue! (or press ESC to quit!)");
+                if (Console.ReadKey().KeyChar == ESC_ASCII_VALUE)
+                    break;
                 TcpClient client = listener.AcceptTcpClient();
-                // Read data from client
                 NetworkStream nwStream = client.GetStream();
+                // Sending data to client
+                string textToSend = "{" + "dateTime:" + DateTime.Now.ToString() + "}";
+                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+                Console.WriteLine("Sending to client: " + Encoding.ASCII.GetString(bytesToSend, 0, bytesToSend.Length));
+
+
+                // Read data from client
                 byte[] buffer = new byte[client.ReceiveBufferSize];
                 int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
                 string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
                 Console.WriteLine("Received from Server: " + dataReceived);
-                // Sending data back to client
-                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes("Capture" + " " + DateTime.Now.ToString());
-                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
-                Console.WriteLine("Sending back : " + Encoding.ASCII.GetString(bytesToSend, 0, bytesToSend.Length));
-                //Console.ReadLine();
+
+                Console.WriteLine("________________________________________________________________________________________");
+
+
             }
             //client.Close();
             //listener.Stop();
