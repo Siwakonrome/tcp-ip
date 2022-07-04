683c8f8de95a7aa5447bf80c2b936e79213bff9d
Siwakon <tiger.prasang@gmail.com>
2022-07-04 16:25:32 +0700

    Update

---
+++ Client/Client.py
@@ -0,0 +1,15 @@
+import socket
+import datetime
+
+HOST = "10.61.5.18"  
+PORT = 80  
+encoding = 'utf-8'
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    dateTime = str(datetime.datetime.now())
+    s.connect((HOST, PORT))
+    s.sendall(dateTime.encode())
+    data = s.recv(1024).decode(encoding)
+
+print(data)
+s.close()
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -10,41 +10,33 @@ namespace Server
     class Program
     {
         const int PORT_NO = 80;
-        const string SERVER_IP = "192.168.101.129";
-        public const byte ESC_ASCII_VALUE = 0x1b;
+        const string SERVER_IP = "10.61.5.18"; 
 
         static void Main(string[] args)
         {
+            TcpClient client;
             IPAddress localAdd = IPAddress.Parse(SERVER_IP);
             TcpListener listener = new TcpListener(localAdd, PORT_NO);
             listener.Start();
-            Console.WriteLine("Server Start...");
             while (true)
             {
-                Console.WriteLine("Press any key to continue! (or press ESC to quit!)");
-                if (Console.ReadKey().KeyChar == ESC_ASCII_VALUE)
-                    break;
-                TcpClient client = listener.AcceptTcpClient();
+                try { client = listener.AcceptTcpClient(); } catch { continue; }
                 NetworkStream nwStream = client.GetStream();
-                // Sending data to client
+                try
+                {
+                    byte[] buffer = new byte[client.ReceiveBufferSize];
+                    int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+                    string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("Received from Server: " + dataReceived);
+                }catch{continue;}
                 string textToSend = "{" + "dateTime:" + DateTime.Now.ToString() + "}";
                 byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
                 nwStream.Write(bytesToSend, 0, bytesToSend.Length);
                 Console.WriteLine("Sending to client: " + Encoding.ASCII.GetString(bytesToSend, 0, bytesToSend.Length));
-
-
-                // Read data from client
-                byte[] buffer = new byte[client.ReceiveBufferSize];
-                int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
-                string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                Console.WriteLine("Received from Server: " + dataReceived);
-
-                Console.WriteLine("________________________________________________________________________________________");
-
-
+                Console.WriteLine("__________________________________________________________________________________");
             }
-            //client.Close();
-            //listener.Stop();
+
+
 
 
         }
