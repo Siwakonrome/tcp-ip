7dbebe4f5ba84d1722acc08f3905996f0d264324
Siwakon <tiger.prasang@gmail.com>
2022-07-04 16:42:43 +0700

    Update

--- Client/Client.py
+++ Client/Client.py
@@ -10,6 +10,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(dateTime.encode())
     data = s.recv(1024).decode(encoding)
-
+    
 print(data)
 s.close()
